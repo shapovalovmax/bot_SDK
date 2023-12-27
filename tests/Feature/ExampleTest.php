25296fc521ecde5889bac2fc5e31d3333092f358
pd-maksymshapovalov <maksym.shapovalov@pinedev.eu>
2023-12-27 16:57:19 +0200

    Finish lesson - 6

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,37 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response()
     {
-        $response = $this->get('/');
+        $json = '{
+    "update_id": "937471378",
+  "message": {
+    "message_id": "20381",
+    "from": {
+        "id": "401741555",
+      "is_bot": "",
+      "first_name": "Максим",
+      "last_name": "Шаповалов",
+      "username": "dramix",
+      "language_code": "uk"
+    },
+    "chat": {
+        "id": "401741555",
+      "first_name": "Максим",
+      "last_name": "Шаповалов",
+      "username": "dramix",
+      "type": "private"
+    },
+    "date": "1703250374",
+    "text": "/start",
+    "entities": {
+        "0": {
+            "offset": "0",
+        "length": "6",
+        "type": "bot_command"
+      }
+    }
+  }
+}';
+        $response = $this->post('/webhook', json_decode($json, true));
 
         $response->assertStatus(200);
     }
